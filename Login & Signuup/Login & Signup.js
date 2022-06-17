107e187c74bd970aff051ab76f6dd5b696fb2f31
Suman kumar <Kumarsumannnnnn@gmail.com>
2022-06-17 16:17:40 +0530

    my first

---
+++ Login & Signuup/Login & Signup.js
@@ -0,0 +1,38 @@
+
+//     function country_code() 
+// {
+//     var val = document.getElementById("contr").value;
+//     if(val=="Select Country")
+//     {
+//         document.getElementById("mobile").value="";
+//     }
+//     else if(val=="India")
+//     {
+//         document.getElementById("mobile").value="+91";
+//     }
+//     else if(val=="U.S")
+//     {
+//         document.getElementById("mobile").value="+1";
+//     }
+// }
+
+
+// var forms = document.querySelector("form");
+// forms.addEventListener("click",function(event){
+//    event.preventDefault();
+//     var data = {
+//          number : forms.number.value,
+//     }
+//     console.log(data);
+// })
+
+
+
+var arr = [];
+
+document.querySelector("form").addEventListener("submit", moBile)
+function moBile(event){
+event.preventDefault();
+
+alert("Otp");
+}
